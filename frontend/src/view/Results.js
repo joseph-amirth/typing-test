788dfa4a0b3d9e5232253853fb2e8353e87f3149
joseph-amirth <epicjoe128@gmail.com>
2024-01-06 16:29:15 +0530

    Use MUI components for results page

--- frontend/src/view/Results.js
+++ frontend/src/view/Results.js
@@ -1,72 +1,107 @@
+import {
+  Pagination,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { Stack } from "@mui/system";
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
-import VerticalSpacer from "../common/VerticalSpacer";
 
-import "./Results.css";
+const PAGE_SIZE = 15;
 
 const Results = () => {
   const params = useLoaderData();
+  const [page, setPage] = useState(1);
+
   const { error } = params;
+  if (error !== undefined) {
+    return <div>{error}</div>;
+  }
+
+  const handlePageChange = (_event, value) => {
+    setPage(value);
+  };
 
   return (
-    <div className="Results">
-      {error === undefined &&
-        params.map((result, i) => <Result key={i} {...result} />)}
-      {error !== undefined && <span className="Error">{error}</span>}
-    </div>
-  );
-};
-
-const Result = ({
-  testParams,
-  testCompletedTimestamp,
-  wpm,
-  rawWpm,
-  accuracy,
-}) => {
-  const { mode, params } = testParams;
-  const datetime = new Date(testCompletedTimestamp * 1000);
-
-  return (
-    <>
-      <div className="Result">
-        <TypingTestParams mode={mode} params={Object.entries(params)} />
-        <Stat name="WPM" value={wpm.toString()} />
-        <Stat name="Accuracy" value={accuracy + "%"} />
-        <Stat name="Raw WPM" value={rawWpm.toString()} />
-        <Stat
-          name="Timestamp"
-          value={
-            datetime.toLocaleDateString() + " " + datetime.toLocaleTimeString()
-          }
-        />
-      </div>
-      <VerticalSpacer />
-    </>
+    <Stack
+      alignItems="center"
+      spacing="1em"
+      sx={{ margin: "0 auto", width: "50%" }}
+    >
+      <Pagination
+        count={Math.ceil(params.length / PAGE_SIZE)}
+        onChange={handlePageChange}
+        color="primary"
+      />
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Mode</TableCell>
+              <TableCell>WPM</TableCell>
+              <TableCell>Accuracy</TableCell>
+              <TableCell>Raw WPM</TableCell>
+              <TableCell>Timestamp</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {params
+              .filter(
+                (_, i) => (page - 1) * PAGE_SIZE <= i && i < page * PAGE_SIZE,
+              )
+              .map(
+                (
+                  { testParams, testCompletedTimestamp, wpm, rawWpm, accuracy },
+                  i,
+                ) => {
+                  return (
+                    <TableRow key={i}>
+                      <TableCell>
+                        <TypingTestParams {...testParams} />
+                      </TableCell>
+                      <TableCell>{wpm}</TableCell>
+                      <TableCell>{accuracy}</TableCell>
+                      <TableCell>{rawWpm}</TableCell>
+                      <TableCell>
+                        {getDateFromTimestampInSecs(testCompletedTimestamp)}
+                      </TableCell>
+                    </TableRow>
+                  );
+                },
+              )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Stack>
   );
 };
 
 const TypingTestParams = ({ mode, params }) => {
+  const stat = (() => {
+    if (mode === "words" || mode === "quote") {
+      return params.length;
+    } else {
+      return params.duration;
+    }
+  })();
+
+  const language = params.language;
+
   return (
     <div className="TypingTestParams">
-      <div className="Mode">{mode}</div>
-      <div className="Params">
-        {params.map(([key, value], i) => (
-          <div key={i} className="Param">
-            {key} {value}
-          </div>
-        ))}
-      </div>
+      {stat} {mode} {language}
     </div>
   );
 };
 
-const Stat = ({ name, value }) => {
-  return (
-    <div className="Stat">
-      <div className="Name">{name}</div>
-      <div className="Value">{value}</div>
-    </div>
-  );
+const getDateFromTimestampInSecs = (timestampInSecs) => {
+  const datetime = new Date(timestampInSecs * 1000);
+  return datetime.toLocaleDateString() + " " + datetime.toLocaleTimeString();
 };
 
 export default Results;
