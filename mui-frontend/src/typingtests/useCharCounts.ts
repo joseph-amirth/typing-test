73f68f0b51a5d84724ebf2f835652f785ee89570
joseph-amirth <epicjoe128@gmail.com>
2024-01-19 23:14:15 +0530

    Migrate char counts hook to ts

--- mui-frontend/src/typingtests/useCharCounts.ts
+++ mui-frontend/src/typingtests/useCharCounts.ts
@@ -1,19 +1,23 @@
 import { useState } from "react";
 
-export const useCharCounts = () => {
+export const useCharCounts = (): [
+  { correctChars: number; incorrectChars: number },
+  (test: string[], previousAttempt: string[], currentAttempt: string[]) => void,
+] => {
   const [correctChars, setCorrectChars] = useState(0);
   const [incorrectChars, setIncorrectChars] = useState(0);
 
   return [
     { correctChars, incorrectChars },
-    (test, previousAttempt, currentAttempt) => {
+    (test: string[], previousAttempt: string[], currentAttempt: string[]) => {
+      let lastIndex = currentAttempt.length - 1;
       if (
         currentAttempt.length === previousAttempt.length &&
-        currentAttempt.at(-1).length > previousAttempt.at(-1).length
+        currentAttempt[lastIndex].length > previousAttempt[lastIndex].length
       ) {
         if (
-          currentAttempt.at(-1).at(-1) ===
-          test[currentAttempt.length - 1][currentAttempt.at(-1).length - 1]
+          currentAttempt[lastIndex].at(-1) ===
+          test[currentAttempt.length - 1][currentAttempt[lastIndex].length - 1]
         ) {
           setCorrectChars(correctChars + 1);
         } else {
