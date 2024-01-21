659387bd2fea65bc89a6bfb69eef117136b5a415
joseph-amirth <epicjoe128@gmail.com>
2024-01-21 21:46:13 +0530

    Stash away some changes

--- mui-frontend/src/typingtests/Diff.tsx
+++ mui-frontend/src/typingtests/Diff.tsx
@@ -1,59 +1,38 @@
+import { useRef } from "react";
 import { disableCutCopyPasteProps } from "../util/component";
 import "./Diff.css";
 
 const Diff = ({
   test,
   attempt,
-  maxCharsInLine,
   showAllLines,
 }: {
   test: string[];
   attempt: string[];
-  maxCharsInLine: number;
   showAllLines: boolean;
 }) => {
-  const diffWords = getDiffWords(test, attempt);
-  const diffLines = getDiffLines(diffWords, maxCharsInLine);
+  const diffDiv = useRef<HTMLDivElement>(null);
 
-  const focusedWordNumber = attempt.length - 1;
+  const diffWords = getDiffWords(test, attempt);
 
   if (showAllLines) {
     return (
       <div className="Diff">
-        {diffLines.map(({ words }, i) => (
-          <Line key={i} words={words} />
+        {diffWords.map((word, i) => (
+          <DiffWord key={i} {...word} />
         ))}
       </div>
     );
   } else {
-    const focusedLineNumber = getFocusedLineNumber(
-      diffLines,
-      focusedWordNumber,
-    );
-    const firstLineNumber = Math.max(0, focusedLineNumber - 1);
-    const displayLines = diffLines.slice(firstLineNumber, firstLineNumber + 3);
-
-    const emptyLine: DiffLineProps = {
-      words: Array<DiffWordProps>(maxCharsInLine).fill({
-        letters: [
-          {
-            letter: " ",
-            state: "Untyped",
-          },
-        ],
-        skipped: false,
-        focused: false,
-      }),
-    };
-
-    while (displayLines.length < 3) {
-      displayLines.push(emptyLine);
-    }
-
     return (
-      <div className="Diff" {...disableCutCopyPasteProps()}>
-        {displayLines.map(({ words }, i) => (
-          <Line key={i} words={words} />
+      <div
+        className="Diff"
+        style={{ height: "3lh", overflow: "hidden" }}
+        ref={diffDiv}
+        {...disableCutCopyPasteProps()}
+      >
+        {diffWords.map((word, i) => (
+          <DiffWord key={i} {...word} />
         ))}
       </div>
     );
@@ -69,10 +48,6 @@ interface DiffWordProps {
   letters: DiffLetterProps[];
   skipped: boolean;
   focused: boolean;
-}
-
-interface DiffLineProps {
-  words: DiffWordProps[];
 }
 
 const Caret = ({ offset }: { offset: number }) => {
@@ -103,16 +78,6 @@ const DiffWord = ({ letters, skipped, focused }: DiffWordProps) => {
         <DiffLetter key={i} {...diffLetterProps} />
       ))}
     </span>
-  );
-};
-
-const Line = ({ words }: DiffLineProps) => {
-  return (
-    <div className="Line">
-      {words.map((diffWordProps, i) => (
-        <DiffWord key={i} {...diffWordProps} />
-      ))}
-    </div>
   );
 };
 
@@ -156,6 +121,11 @@ function getDiffWords(test: string[], attempt: string[]): DiffWordProps[] {
     const skipped =
       !focused && letters.some(({ state }) => state !== "Correct");
     diffWords.push({ letters, skipped, focused });
+    diffWords.push({
+      letters: [{ letter: " ", state: "Untyped" }],
+      skipped: false,
+      focused: false,
+    });
   }
 
   for (let i = attempt.length; i < test.length; i++) {
@@ -165,71 +135,14 @@ function getDiffWords(test: string[], attempt: string[]): DiffWordProps[] {
       letters.push({ letter, state: "Untyped" });
     }
     diffWords.push({ letters, skipped: false, focused: false });
+    diffWords.push({
+      letters: [{ letter: " ", state: "Untyped" }],
+      skipped: false,
+      focused: false,
+    });
   }
 
   return diffWords;
 }
-
-function getDiffLines(
-  diffWords: DiffWordProps[],
-  maxCharsInLine: number,
-): DiffLineProps[] {
-  const lines: DiffLineProps[] = [];
-
-  let currentLine: DiffWordProps[] = [];
-  let currentLineChars = 0;
-
-  const space: DiffWordProps = {
-    letters: [{ letter: " ", state: "Untyped" }],
-    skipped: false,
-    focused: false,
-  };
-
-  for (let i = 0; i < diffWords.length; i++) {
-    const len = diffWords[i].letters.length;
-    if (currentLineChars + len + 1 <= maxCharsInLine) {
-      currentLine.push(diffWords[i]);
-      currentLine.push(space);
-      currentLineChars += len + 1;
-    } else if (currentLineChars + len <= maxCharsInLine) {
-      currentLine.push(diffWords[i]);
-      currentLineChars += len;
-    } else {
-      // Fill remainder of the line with spaces.
-      while (currentLineChars < maxCharsInLine) {
-        currentLine.push(space);
-        currentLineChars += 1;
-      }
-      lines.push({ words: currentLine });
-      // Reset current line.
-      currentLine = [diffWords[i], space];
-      currentLineChars = len + 1;
-    }
-  }
-
-  lines.push({ words: currentLine });
-
-  return lines;
-}
-
-const getFocusedLineNumber = (
-  diffLines: DiffLineProps[],
-  focusedWordNumber: number,
-): number => {
-  let currentWordNumber = 0;
-  for (let lineNumber = 0; lineNumber < diffLines.length; lineNumber++) {
-    const { words } = diffLines[lineNumber];
-    for (const { letters } of words) {
-      if (letters.length === 1 && letters[0].letter === " ") {
-        continue;
-      }
-      if (currentWordNumber === focusedWordNumber) {
-        return lineNumber;
-      }
-      currentWordNumber += 1;
-    }
-  }
-  return diffLines.length - 1;
-};
 
 export default Diff;
