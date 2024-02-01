77bd32b8ae557aef7cfc2442f0cd92dfb68ef1f0
joseph-amirth <epicjoe128@gmail.com>
2024-02-01 20:31:07 +0530

    Make backend functions non-arrow functions

--- frontend/src/util/backend.ts
+++ frontend/src/util/backend.ts
@@ -8,49 +8,49 @@ import { getOrInitItem } from "./local-storage";
 const backendUrl = "http://localhost:8080";
 
 // Returned promise resolves to user details and settings on success.
-export const signUp = async (params: {
+export async function signUp(params: {
   username: string;
   email: string;
   password: string;
   preferences: Preferences;
-}) => {
+}) {
   return postJson("/signup", params, { credentials: "include" }).then(
     extractJson,
   );
-};
+}
 
-export const signInWithEmail = async ({
+export async function signInWithEmail({
   email,
   password,
 }: {
   email: string;
   password: string;
-}) => {
+}) {
   return signIn({ usernameOrEmail: { email }, password });
-};
+}
 
-export const signInWithUsername = async ({
+export async function signInWithUsername({
   username,
   password,
 }: {
   username: string;
   password: string;
-}) => {
+}) {
   return signIn({ usernameOrEmail: { username }, password });
-};
+}
 
 // Returned promise resolves to user details and settings on success.
-const signIn = async (params: {
+async function signIn(params: {
   usernameOrEmail: { username: string } | { email: string };
   password: string;
-}) => {
+}) {
   return postJson("/signin", params, { credentials: "include" }).then(
     extractJson,
   );
-};
+}
 
 // Returned promise resolves to user details and preferences on success.
-export const currentUser = async () => {
+export async function currentUser() {
   return get("/current", { credentials: "include" }).then((response) => {
     if (response.status !== 200) {
       return {
@@ -59,19 +59,19 @@ export const currentUser = async () => {
     }
     return response.json();
   });
-};
+}
 
-export const logout = async () => {
+export async function logout() {
   return get("/logout", { credentials: "include" });
-};
+}
 
-export const postResult = async (params: {
+export async function postResult(params: {
   testParams: TypingTestParams;
   testCompletedTimestamp: number;
   wpm: number;
   rawWpm: number;
   accuracy: number;
-}) => {
+}) {
   return postJson("/result", params, { credentials: "include" }).then(
     (response) => {
       if (response.status !== 200) {
@@ -81,30 +81,30 @@ export const postResult = async (params: {
       }
     },
   );
-};
+}
 
-export const getResults = async () => {
+export async function getResults() {
   return get("/result", { credentials: "include" }).then(extractJson);
-};
+}
 
-export const updatePreferences = async (preferences: Preferences) => {
+export async function updatePreferences(preferences: Preferences) {
   return postJson("/prefs", preferences, { credentials: "include" });
-};
+}
 
-const get = async (path: string, options = {}) => {
+async function get(path: string, options = {}) {
   return fetchNonThrowing(`${backendUrl}${path}`, options);
-};
+}
 
-const extractJson = async (response: Response) => {
+async function extractJson(response: Response) {
   if (!response.ok) {
     return response.text().then((text) => {
       return { error: text };
     });
   }
   return response.json();
-};
+}
 
-const postJson = async (path: string, obj: object, options = {}) => {
+async function postJson(path: string, obj: object, options = {}) {
   return fetchNonThrowing(`${backendUrl}${path}`, {
     method: "POST",
     headers: {
@@ -113,13 +113,13 @@ const postJson = async (path: string, obj: object, options = {}) => {
     body: JSON.stringify(obj),
     ...options,
   });
-};
+}
 
-const fetchNonThrowing = async (path: string, options = {}) => {
+async function fetchNonThrowing(path: string, options = {}) {
   return fetch(path, options).catch(() => {
     return new Response(null, {
       status: 503,
       statusText: "Service unavailable",
     });
   });
-};
+}
