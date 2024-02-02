e328d01b95fba92991ab4fe0920d2b28e45c425d
joseph-amirth <epicjoe128@gmail.com>
2024-02-02 20:45:09 +0530

    Make specific tests work again

--- frontend/src/typing-test/quote/SpecificTypingTest.tsx
+++ frontend/src/typing-test/quote/SpecificTypingTest.tsx
@@ -1,21 +1,26 @@
 import { useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { copyTextToClipboard } from "../../util/misc";
 import BoundedTypingTest from "../BoundedTypingTest";
 import Buttons from "./../Buttons";
 import quotesInfo from "../../static/quotes.json";
 import "./SpecificTypingTest.css";
 import VerticalSpacer from "../../common/VerticalSpacer";
+import { QuoteModeLength } from "../../context/preference";
 
 const getQuote = (quoteId: number) => {
   return quotesInfo.quotes[quoteId].text;
 };
 
-const SpecificTypingTest = () => {
+const SpecificTypingTest = ({
+  id,
+}: {
+  length: QuoteModeLength;
+  id: number;
+}) => {
   const navigate = useNavigate();
-  const { id } = useParams() as { id: string };
 
-  const quote = getQuote(parseInt(id));
+  const quote = getQuote(id);
 
   const [key, setKey] = useState(Date.now());
 
--- frontend/src/view/SpecificTypingTestView.tsx
+++ frontend/src/view/SpecificTypingTestView.tsx
@@ -1,13 +1,62 @@
-import { Language } from "../context/languages";
+import { Language, useLanguage } from "../context/languages";
 import { Seed } from "../util/prng";
 import { QuoteModeLength } from "../context/preference";
+import { useLoaderData } from "react-router-dom";
+import WordsTypingTest from "../typing-test/words/SeededTypingTest";
+import TimeTypingTest from "../typing-test/time/SeededTypingTest";
+import QuoteTypingTest from "../typing-test/quote/SpecificTypingTest";
 
 // TODO: Implement this.
 function SeededTypingTestView() {
-  return <div className="SeededTypingTestView"></div>;
+  const loaderData = useLoaderData() as SeededTypingTestParams;
+
+  return (
+    <div className="SeededTypingTestView">{getTypingTestView(loaderData)}</div>
+  );
 }
 
-// @ts-ignore unused type.
+function getTypingTestView({ mode, params }: SeededTypingTestParams) {
+  switch (mode) {
+    case "words":
+      return <WordsTypingTestView {...params} />;
+    case "time":
+      return <TimeTypingTestView {...params} />;
+    case "quote":
+      return <QuoteTypingTestView {...params} />;
+  }
+}
+
+function WordsTypingTestView({
+  language,
+  length,
+  seed,
+}: WordsSeededTypingTestParams["params"]) {
+  const words = useLanguage(language);
+  if (words === undefined) {
+    return;
+  }
+  return <WordsTypingTest words={words} length={length} seed={seed} />;
+}
+
+function TimeTypingTestView({
+  language,
+  duration,
+  seed,
+}: TimeSeededTypingTestParams["params"]) {
+  const words = useLanguage(language);
+  if (words === undefined) {
+    return;
+  }
+  return <TimeTypingTest words={words} duration={duration} seed={seed} />;
+}
+
+function QuoteTypingTestView({
+  length,
+  id,
+}: QuoteSeededTypingTestParams["params"]) {
+  return <QuoteTypingTest length={length} id={id} />;
+}
+
 type SeededTypingTestParams =
   | WordsSeededTypingTestParams
   | TimeSeededTypingTestParams
@@ -27,7 +76,7 @@ export interface TimeSeededTypingTestParams {
   params: {
     language: Language;
     duration: number;
-    id: number;
+    seed: Seed;
   };
 }
 
