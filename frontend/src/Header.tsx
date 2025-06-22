069b45d132e9f5d00c6580f8bb1d1f9c2e1ec905
joseph-amirth <epicjoe128@gmail.com>
2025-06-22 10:13:04 +0530

    Replace profile and log-out MUI buttons with custom anchor tags.

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -3,16 +3,15 @@ import SportsScoreIcon from "./static/icons/sports_score.svg?react";
 import LeaderboardIcon from "./static/icons/leaderboard.svg?react";
 import SettingsIcon from "./static/icons/settings.svg?react";
 import LoginIcon from "./static/icons/login.svg?react";
+import LogoutIcon from "./static/icons/logout.svg?react";
 import PersonAddIcon from "./static/icons/person_add.svg?react";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Header.css";
-import { Button } from "@mui/material";
 import { Account, AccountService } from "./service/account";
 import { useService } from "./service";
 
 function Header() {
-  const navigate = useNavigate();
   const { accountState } = useService(AccountService);
 
   return (
@@ -56,7 +55,6 @@ function NavBar() {
 }
 
 function UserDetails({ account }: { account: Account }) {
-  const navigate = useNavigate();
   const accountService = useService(AccountService);
 
   const [loading, setLoading] = useState(false);
@@ -68,14 +66,12 @@ function UserDetails({ account }: { account: Account }) {
 
   return (
     <div className="UserDetails">
-      <Button onClick={() => navigate("/profile")}>{account.username}</Button>{" "}
-      <Button
-        variant="contained"
-        onClick={handleLogOutButtonClick}
-        disabled={loading}
-      >
-        Log out
-      </Button>
+      <Link to="/profile" className="Link">
+        {account.username}
+      </Link>
+      <button onClick={handleLogOutButtonClick} disabled={loading}>
+        <LogoutIcon />
+      </button>
     </div>
   );
 }
